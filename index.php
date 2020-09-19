71b6a13eb29970cb5a5143fa7ef127e5e6994757
Mon <reymondturbanada2@gmail.com>
2020-09-19 22:40:57 +0800

    Basic Fundamentals Of PHP Part 2

--- index.php
+++ index.php
@@ -91,6 +91,104 @@ switch ($monmamon) {
         break;
 }
 
+// 9 - Loop
+//WHILE loop
+$a = 1;
+while ($a <= 5) {
+    echo 'Counter ' . $a . '<br>';
+    $a++;
+}
+
+//DO WHILE (kahit mag false na, ginagawa nya pa rin ang nasa loop)
+$b = 6;
+do {
+    echo 'Counter: ' . $b . '<br>';
+    $b++;
+} while ($b <= 5);
+
+// For loop
+for ($i=0; $i <= 5 ; $i++) { 
+    echo 'Counter: '. $i .'<br>';
+}
+
+// Foreach (ginagamit sa array)
+$test_array = ['Code', 'with', 'Monmon'];
+foreach ($test_array as $value) {
+    echo 'Value: ' .$value . '<br>';
+}
+
+//using and getting the index
+foreach ($test_array as $index => $value) {
+    echo 'Value: ' . $index . ' : ' . $value . '<br>';
+}
+
+$test_array_two = array('Code', 'with', 'rIndex' =>'Mon');
+foreach ($test_array_two as $index => $value) {
+    echo 'Value: ' . $index . ' : ' . $value . '<br>';
+}
+
+// 10 - Forms (kukunin ang value sa forms)
+if (isset($_POST['submit'])){
+    // $name = $_GET['name'];
+    // $age = $_GET['age'];
+    
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+
+    echo "Name: $name <br>";
+    echo "Age: $age <br>";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <form action="/PHP_Tutorial_for_Github/index.php" method="get"> -->
+    <form action="/PHP_Tutorial_for_Github/index.php" method="post">
+        <input type="text" name="name" id="" placeholder="Enter a name">
+        <input type="text" name="age" id="" placeholder="Enter an age">
+        <input type="submit" value="CLICK">
+    </form>    
+</body>
+</html>
+
+<?php
+// 11 - MySQL
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = '';
+
+// create a connection
+$connection = mysqli_connect($host, $user, $password, $database);
+
+$query_user = "SELECT * FROM user";
+// $sql_user = mysqli_query($connection, $query_user);
+$sql_user = $connection->query($query_user);
+
+// While loop
+while($results = mysqli_fetch_array($sql_user)) {
+    echo 'Name: '.$results['name'].'<br>';
+    echo 'Age: '.$results['age'].'<br>';
+}
+
+// For Loop
+for ($i=1; $i <= mysqli_num_rows($sql_user) ; $i++) { 
+    $results = mysqli_fetch_array($sql_user);
+
+    echo 'Name: '.$results['name'].'<br>';
+    echo 'Age: '.$results['age'].'<br>';
+}
+
+
+
+
+
 
 
 
